4b17d13204b65848804059275fb950cc121583c5
AtsushiYamashita <yays505@gmail.com>
2017-11-25 21:35:46 +0900

    Add stage interface.

---
+++ Assets/Yamashita/Scripts/IStage.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Stage data 
+/// </summary>
+public interface IStage {
+
+    /// <summary>
+    /// Return stage level.
+    /// </summary>
+    /// <returns>Stage level</returns>
+    int GetLevel ();
+
+    /// <summary>
+    /// Return game over time from app start.
+    /// </summary>
+    /// <returns></returns>
+    float GetTimeLimit ();
+
+    /// <summary>
+    /// Return Stage Start time
+    /// </summary>
+    /// <returns>Stage Start time</returns>
+    float GetStartTime ();
+}

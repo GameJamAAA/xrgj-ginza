6532b85a07d999c6c344a2d7d249e8388968e3b8
AtsushiYamashita <yays505@gmail.com>
2017-11-25 21:37:01 +0900

    Add singleton game data manager.

---
+++ Assets/Yamashita/Scripts/GameDataManager.cs
@@ -0,0 +1,50 @@
+﻿/// <summary>
+/// Game data manager as singleton.
+/// </summary>
+public class GameDataManager: IGameManager {
+
+    /// <summary>
+    /// Handle instance.
+    /// </summary>
+    private static IGameManager mInstance = null;
+
+    /// <summary>
+    /// Seal for singleton
+    /// </summary>
+    private GameDataManager () { }
+
+    /// <summary>
+    /// Return singleton instance.
+    /// </summary>
+    /// <returns></returns>
+    public static IGameManager Instance () {
+        if (mInstance == null) {
+            var ins = new GameDataManager().Init();
+            mInstance = ins;
+        }
+        return mInstance;
+    }
+
+    public int GetScore () {
+        throw new System.NotImplementedException();
+    }
+
+    public IStage GetStage () {
+        throw new System.NotImplementedException();
+    }
+
+    public State GetState () {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Initialize in created time.
+    /// </summary>
+    /// <returns>Initialized instance.</returns>
+    private IGameManager Init () {
+
+        if (mInstance != null) { return this; }
+
+        return this;
+    }
+}
